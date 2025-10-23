ec11c02f589183e99279326024d239594d6eb5fd
Pawan Mishra <pawanmishra75066@gmail.com>
2025-10-24 01:12:45 +0530

    Fix duplicate navbar issue by restructuring App.jsx to use a layout component that renders the navbar once

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,6 +8,14 @@ import AdminLogin from './components/Login/Login';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import AdminNavbar from './components/Navbar/Navbar';
 
+// Layout component that includes the navbar
+const Layout = ({ children }) => (
+  <div>
+    <AdminNavbar />
+    {children}
+  </div>
+);
+
 function App() {
   return (
     <div>
@@ -15,26 +23,23 @@ function App() {
         <Route path="/login" element={<AdminLogin />} />
         <Route path="/" element={
           <ProtectedRoute>
-            <>
-              <AdminNavbar />
+            <Layout>
               <AddItems />
-            </>
+            </Layout>
           </ProtectedRoute>
         } />
         <Route path="/list" element={
           <ProtectedRoute>
-            <>
-              <AdminNavbar />
+            <Layout>
               <ListItems />
-            </>
+            </Layout>
           </ProtectedRoute>
         } />
         <Route path="/orders" element={
           <ProtectedRoute>
-            <>
-              <AdminNavbar />
+            <Layout>
               <Orders />
-            </>
+            </Layout>
           </ProtectedRoute>
         } />
         {/* Redirect any unknown routes to the dashboard */}
