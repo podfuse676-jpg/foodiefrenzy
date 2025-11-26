c3bd54431ee5cd588a5e5533b947bbaa9a2a03a6
Pawan Mishra <pawanmishra75066@gmail.com>
2025-11-26 21:07:35 +0530

    Fix HTML structure in admin App.jsx to remove extra div and potential spacing issues

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -16,28 +16,26 @@ function App() {
     <div>
       {/* Navbar is always visible except on login page */}
       <AdminNavbar />
-      <div className="pt-20">
-        <Routes>
-          <Route path="/login" element={<AdminLogin />} />
-          <Route path="/" element={
-            <ProtectedRoute>
-              <AddItems />
-            </ProtectedRoute>
-          } />
-          <Route path="/list" element={
-            <ProtectedRoute>
-              <ListItems />
-            </ProtectedRoute>
-          } />
-          <Route path="/orders" element={
-            <ProtectedRoute>
-              <Orders />
-            </ProtectedRoute>
-          } />
-          {/* Redirect any unknown routes to the dashboard */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <AddItems />
+          </ProtectedRoute>
+        } />
+        <Route path="/list" element={
+          <ProtectedRoute>
+            <ListItems />
+          </ProtectedRoute>
+        } />
+        <Route path="/orders" element={
+          <ProtectedRoute>
+            <Orders />
+          </ProtectedRoute>
+        } />
+        {/* Redirect any unknown routes to the dashboard */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </div>
   );
 }
