a362f02edf865861ae45daefc044ea4e4e9bc14e
Pawan Mishra <pawanmishra75066@gmail.com>
2025-11-29 14:12:12 +0530

    Fix import path for apiConfig in EmailLogin component

--- frontend/src/pages/EmailLogin/EmailLogin.jsx
+++ frontend/src/pages/EmailLogin/EmailLogin.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import apiConfig from '../utils/apiConfig';
+import apiConfig from '../../utils/apiConfig';
 import {
   FaEnvelope,
   FaLock,
