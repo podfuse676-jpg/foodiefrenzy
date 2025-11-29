23dd2b2fa1b7cd8751a37480c58f912356631e5d
Pawan Mishra <pawanmishra75066@gmail.com>
2025-11-30 00:48:38 +0530

    Trigger redeployment of admin panel to fix API URL

--- admin/src/utils/apiConfig.js
+++ admin/src/utils/apiConfig.js
@@ -2,4 +2,4 @@ const apiConfig = {
   baseURL: import.meta.env.VITE_API_URL || 'https://lakeshore-convenience.onrender.com'
 };
 
-export default apiConfig;
+export default apiConfig;// Trigger redeployment
