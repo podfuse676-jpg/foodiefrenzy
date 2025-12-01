63dcb01018b39c4983777fb860430f0e6e85edf2
Pawan Mishra <pawanmishra75066@gmail.com>
2025-12-01 20:20:13 +0530

    Fix build by removing Users component references

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import AddItems from './components/AddItems/AddItems';
 import Orders from './components/Orders/Orders';
 import ListItems from './components/ListItems/ListItems';
-import Users from './components/Users/Users';
 import AdminLogin from './components/Login/Login';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import AdminNavbar from './components/Navbar/Navbar';
@@ -32,11 +31,6 @@ function App() {
         <Route path="/orders" element={
           <ProtectedRoute>
             <Orders />
-          </ProtectedRoute>
-        } />
-        <Route path="/users" element={
-          <ProtectedRoute>
-            <Users />
           </ProtectedRoute>
         } />
         {/* Redirect any unknown routes to the dashboard */}
